1f1655c7225e2c18a3ca6a673ccf50f7b61e84a4
Marius Wachtler <undingen@gmail.com>
2016-05-19 16:01:31 +0100

    optimized _PyObject_GC_TRACK

--- from_cpython/Include/objimpl.h
+++ from_cpython/Include/objimpl.h
@@ -271,14 +271,14 @@ extern PyGC_Head *_PyGC_generation0;
 /* Tell the GC to track this object.  NB: While the object is tracked the
  * collector it must be safe to call the ob_traverse method. */
 #define _PyObject_GC_TRACK(o) do { \
-    PyGC_Head *g = _Py_AS_GC(o); \
-    if (g->gc.gc_refs != _PyGC_REFS_UNTRACKED) \
+    PyGC_Head *g = _Py_AS_GC(o), *PyGC_generation0 = _PyGC_generation0, *prev = PyGC_generation0->gc.gc_prev; \
+    if (__builtin_expect(g->gc.gc_refs != _PyGC_REFS_UNTRACKED, 0)) \
         Py_FatalError("GC object already tracked"); \
+    PyGC_generation0->gc.gc_prev = g; \
     g->gc.gc_refs = _PyGC_REFS_REACHABLE; \
-    g->gc.gc_next = _PyGC_generation0; \
-    g->gc.gc_prev = _PyGC_generation0->gc.gc_prev; \
-    g->gc.gc_prev->gc.gc_next = g; \
-    _PyGC_generation0->gc.gc_prev = g; \
+    g->gc.gc_next = PyGC_generation0; \
+    g->gc.gc_prev = prev; \
+    prev->gc.gc_next = g; \
     } while (0);
 
 /* Tell the GC to stop tracking this object.
