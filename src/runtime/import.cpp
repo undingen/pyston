525a4327fe09a45df563d60d2351bf4c54b8c37a
Marius Wachtler <undingen@gmail.com>
2016-09-13 18:39:43 +0100

    f

--- src/runtime/import.cpp
+++ src/runtime/import.cpp
@@ -133,7 +133,9 @@ extern "C" PyObject* PyImport_ExecCodeModuleEx(const char* name, PyObject* co, c
     }
 }
 
-extern "C" Box* import(int level, Box* from_imports, BoxedString* module_name) {
+extern "C" Box* import(int level, Box* from_imports, Box* _module_name) {
+    assert(_module_name->cls == str_cls);
+    BoxedString* module_name = (BoxedString*)_module_name;
     Box* rtn = PyImport_ImportModuleLevel(module_name->c_str(), getGlobalsDict(), NULL, from_imports, level);
     if (!rtn)
         throwCAPIException();
